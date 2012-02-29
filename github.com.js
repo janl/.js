8bf4b2579659473a10385c51ab2f7bcc52b908fc
Jan Lehnardt <jan@apache.org>
2012-02-29 13:24:29 +0100

    detect gpl in COPYING files

--- github.com.js
+++ github.com.js
@@ -1,9 +1,21 @@
+/*
+   GitHub GPL Warner
+   Credit https://twitter.com/#!/tcurdt/status/86193273055612929
+   (c) Jan Lehnardt 2011 <jan@apache.org>
+   MIT License
+*/
 // if I'm in a repo
 var slashcount = window.location.href.split("/").length;
 if(slashcount >= 5) { // we are in a repo
   // find a top level LICENSE* file
-  var licenseurl = $('a[href*="LICENSE"]').attr("href");
-  $.get(licenseurl, null, function(data, status, result) {
+  var license_files = ["LICENSE", "COPYING"];
+  var licenseurl = false;
+
+  license_files.forEach(function(license_file) {
+    licenseurl = $('a[href*="' + license_file + '"]').attr("href");
+  });
+
+  licenseurl && $.get(licenseurl, null, function(data, status, result) {
     if(data.match(/gpl/i)) { // grep it for *GPL*
       // paint red tape
       $("body").prepend($("<h2>Watch out, this repository contains GPL licensed code!</h2>")

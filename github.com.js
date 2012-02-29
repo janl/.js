b90f4570a556cbd85c20fb225e9aa553ffc8e201
Jan Lehnardt <jan@apache.org>
2012-02-29 13:24:43 +0100

    add git fetch helper!

--- github.com.js
+++ github.com.js
@@ -34,5 +34,28 @@ if(slashcount >= 5) { // we are in a repo
   });
 }
 
-// credit https://twitter.com/#!/tcurdt/status/86193273055612929
+/*
+  Pull request helper
+   (c) Jan Lehnardt 2011 <jan@apache.org>
+   MIT License
+*/
 
+//https://github.com/janl/mustache.js/pull/177
+//git fetch https://github.com/subzey/mustache.js.git && git cherry-pick FETCH_HEAD
+var path_parts = window.location.href.split("/");
+if(path_parts[5] && path_parts[5] == "pull") {
+  // we are in a pull request
+  var repo = path_parts[4];
+  var ref = $('span[class*="commit-ref from"]').text().split(":");
+  if(ref.length == 1) { // fetch form a branch our own repo!
+    var user = path_parts[3];
+    var branch = ref[0];
+  } else {
+    var user = ref[0];
+    var branch = ref[1];
+  }
+  var fetch = "git fetch https://github.com/" + user +"/" + repo + ".git +refs/heads/" + branch +" && git cherry-pick FETCH_HEAD"
+  var tabs = $('ul[class*="js-hard-tabs  smalltabs"]');
+  var li = $('<div> One stop fetch: ' + fetch +'</fiv>');
+  $(tabs).after(li);
+}
